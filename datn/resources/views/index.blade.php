9269132c59e54482edebcfe3c5545d8cb836cd80
lethanhtu030902 <lethanhtu030902@gmail.com>
2023-04-24 23:59:18 +0700

    edit book 2

--- datn/resources/views/index.blade.php
+++ datn/resources/views/index.blade.php
@@ -406,19 +406,19 @@
                 </div>
             </div>
             <!-- IDEA2 -->
-            <div class="wrap-thuc-don padding50"> 
-                <div class="wrap-content"> 
-                    <div class="wrap_title"> 
-                        <div class="title-main-thuc-don"><span>BOOK</span></div>  
-                    </div> 
-                    <div class="book-menu position-relative padding30"> 
-                        <div id="container"> 
-                            <div id="main"> 
-                                <div id="features"> 
-                                <div><img src="./assets/images/slideshow/slide.png" alt=""></div>
-                    <div><img src="./assets/images/slideshow/banner_02.png" alt=""></div>
-                    <div><img src="./assets/images/slideshow/slide1.png" alt=""></div>
-                    <div><img src="./assets/images/slideshow/slideshow_02.png" alt=""></div>
+            <div class="wrap-thuc-don padding50">
+                <div class="wrap-content">
+                    <div class="wrap_title">
+                        <div class="title-main-thuc-don"><span>BOOK</span></div>
+                    </div>
+                    <div class="book-menu position-relative padding30">
+                        <div id="container">
+                            <div id="main">
+                                <div id="features">
+                                    <div><img src="./assets/images/slideshow/slide.png" alt=""></div>
+                                    <div><img src="./assets/images/slideshow/banner_02.png" alt=""></div>
+                                    <div><img src="./assets/images/slideshow/slide1.png" alt=""></div>
+                                    <div><img src="./assets/images/slideshow/slideshow_02.png" alt=""></div>
                                 </div>
                             </div>
                         </div>
@@ -435,7 +435,7 @@
                         </nav>
                     </div>
 
-                    <div class="book-menu-mobile padding30">
+                    <!-- <div class="book-menu-mobile padding30">
                         <div class="owl-page owl-carousel owl-theme" data-xsm-items="1:0" data-sm-items="1:0"
                             data-md-items="1:0" data-lg-items="1:0" data-xlg-items="1:0" data-rewind="1"
                             data-autoplay="1" data-loop="0" data-lazyload="0" data-mousedrag="1" data-touchdrag="1"
@@ -445,11 +445,11 @@
                             data-navtext="<svg xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-arrow-narrow-left' width='50' height='37' viewBox='0 0 24 24' stroke-width='1' stroke='#ffffff' fill='none' stroke-linecap='round' stroke-linejoin='round'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><line x1='5' y1='12' x2='19' y2='12' /><line x1='5' y1='12' x2='9' y2='16' /><line x1='5' y1='12' x2='9' y2='8' /></svg>|<svg xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-arrow-narrow-right' width='50' height='37' viewBox='0 0 24 24' stroke-width='1' stroke='#ffffff' fill='none' stroke-linecap='round' stroke-linejoin='round'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><line x1='5' y1='12' x2='19' y2='12' /><line x1='15' y1='16' x2='19' y2='12' /><line x1='15' y1='8' x2='19' y2='12' /></svg>"
                             data-navcontainer=".control-video">
                             <div><img src="./assets/images/slideshow/slide.png" alt=""></div>
-                    <div><img src="./assets/images/slideshow/banner_02.png" alt=""></div>
-                    <div><img src="./assets/images/slideshow/slide1.png" alt=""></div>
-                    <div><img src="./assets/images/slideshow/slideshow_02.png" alt=""></div>
+                            <div><img src="./assets/images/slideshow/banner_02.png" alt=""></div>
+                            <div><img src="./assets/images/slideshow/slide1.png" alt=""></div>
+                            <div><img src="./assets/images/slideshow/slideshow_02.png" alt=""></div>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
 
             </div>
