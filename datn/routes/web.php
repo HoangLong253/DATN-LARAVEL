89fcff0420cd4578f11293d09013b11690db256a
HoangLong253 <0306201253@caothang.edu.vn>
2023-05-17 23:38:49 +0700

    ._. làm việc thôi Tú ơi ko là năm sau làm đấy @Tú

--- datn/routes/web.php
+++ datn/routes/web.php
@@ -20,6 +20,22 @@ Route::get('dangki', function () {
     return view('DangKy');
 })->name('dangki');
 
+Route::get('taphop', function () {
+    return view('taphop');
+})->name('taphop');
+
+//này là taphop/cái gì đó vd taphop/sach-giao-khoa
+
+/*Route::get('taphop', function () {
+    return view('taphop');
+})->name('taphop');*/
+
+//này là taphop/sach-giao-khoa/cái gì đó vd taphop/sach-giao-khoa/lop-6/toan-6-tap-1
+
+/*Route::get('taphop', function () {
+    return view('taphop');
+})->name('taphop');*/
+
 /*Route::post('validate_registration', 'validate_registration')->name('logreg.validate_registration');
 
 Route::post('validate_login', 'validate_login')->name('logreg.validate_login');*/
