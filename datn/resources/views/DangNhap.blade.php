9e64fa99e203d9ae773ab6008949e2b7e8abfec0
lethanhtu030902 <lethanhtu030902@gmail.com>
2023-05-26 23:27:59 +0700

    gio hang

--- datn/resources/views/DangNhap.blade.php
+++ datn/resources/views/DangNhap.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dedicated Dragon BookStore</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
+ 
 </head>
 <body>
 
@@ -75,6 +76,7 @@
         </div>
         
     </div>
+    
     
 </body>
 </html>
