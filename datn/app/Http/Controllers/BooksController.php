81b2885261fee104f428c13f0e4d6360f838838a
HoangLong253 <0306201253@caothang.edu.vn>
2023-07-03 11:03:03 +0700

    sửa lại truy vấn

--- datn/app/Http/Controllers/BooksController.php
+++ datn/app/Http/Controllers/BooksController.php
@@ -281,7 +281,7 @@ class BooksController extends Controller
         $search_value = DB::table('saches')
                         ->where('TenSach', 'LIKE', '%' . $var . '%')
                         ->get();
-        if(isset($_COOKIE['is_logged']) || $_COOKIE['is_logged']==1 ) {
+        if(isset($_COOKIE['is_logged']) && $_COOKIE['is_logged']==1 ) {
             $user1 = DB::table("nguoidung")->where('MaNgDung', '=' ,$_COOKIE['id'])->get();
             return view('timkiem', [
                 'user1' => $user1,
