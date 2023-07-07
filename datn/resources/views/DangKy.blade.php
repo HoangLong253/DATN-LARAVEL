59f15dab2409af451b023bdb8b3fdc9d680dc8fd
lethanhtu030902 <lethanhtu030902@gmail.com>
2023-07-07 18:30:22 +0700

    effect

--- datn/resources/views/DangKy.blade.php
+++ datn/resources/views/DangKy.blade.php
@@ -34,39 +34,42 @@
         </div>
     </nav>
     <div class="container mt-5">
-<!-- Đăng ký -->
-<div class="wrap-user">
-    <div class="return">
-        @if ($message = Session::get('success'))
-            <div>
-                <div style="color: #12c300;
+        <!-- Đăng ký -->
+        <div class="wrap-userdk">
+            <div class="box-regis">
+                <div class="return">
+                    @if ($message = Session::get('success'))
+                    <div>
+                        <div style="color: #12c300;
             font-size: 1.2em;font-weight: bold;">{{ $message }}</div>
-            </div>
-        @endif
-        @if ($message = Session::get('fail'))
-            <div>
-                <div style="color: #dd0505;
+                    </div>
+                    @endif
+                    @if ($message = Session::get('fail'))
+                    <div>
+                        <div style="color: #dd0505;
             font-size: 1.2em;font-weight: bold;">{{ $message }}</div>
-            </div>
-        @endif
-    </div>
-        <div class="title-user">
-            <span>Đăng ký</span>
-        </div>
-        <form class="form-user validation-user" novalidate="" method="post" action="{{route('check_register')}}" enctype="multipart/form-data">
-            @csrf
-            <label>Họ tên</label>
-            <div class="input-group input-user">
-                <div class="input-group-prepend">
-                    <div class="input-group-text"><i class="fa fa-user"></i></div>
+                    </div>
+                    @endif
                 </div>
-                <input type="text" class="form-control text-sm" id="fullname" name="fullname" placeholder="Nhập họ tên của bạn" value="" required="">
-            <div class="invalid-feedback">Vui lòng nhập họ và tên</div>
-            </div>
-            @error('fullname')
-                <p class="">{{ $message }}</p>
-            @enderror
-            <!--<label>Tài khoản</label>
+                <div class="title-user">
+                    <span>Đăng ký</span>
+                </div>
+                <form class="form-user validation-user" novalidate="" method="post" action="{{route('check_register')}}"
+                    enctype="multipart/form-data">
+                    @csrf
+                    <label>Họ tên</label>
+                    <div class="input-group input-user">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fa fa-user"></i></div>
+                        </div>
+                        <input type="text" class="form-control text-sm" id="fullname" name="fullname"
+                            placeholder="Nhập họ tên của bạn" value="" required="">
+                        <div class="invalid-feedback">Vui lòng nhập họ và tên</div>
+                    </div>
+                    @error('fullname')
+                    <p class="">{{ $message }}</p>
+                    @enderror
+                    <!--<label>Tài khoản</label>
             <div class="input-group input-user">
                 <div class="input-group-prepend">
                     <div class="input-group-text"><i class="fa fa-user"></i></div>
@@ -74,51 +77,55 @@
                 <input type="text" class="form-control text-sm" id="username" name="username" placeholder="Nhập tài khoản của bạn" value="" required="">
             <div class="invalid-feedback">Vui lòng nhập tài khoản</div>
             </div>-->
-            <label>Mật khẩu</label>
-            <div class="input-group input-user">
-                <div class="input-group-prepend">
-                    <div class="input-group-text"><i class="fa fa-lock"></i></div>
-                </div>
-                <input type="password" class="form-control text-sm" id="password" name="password" placeholder="Nhập mật khẩu (trên 8 kí tự)" required="">
-            <div class="invalid-feedback">Vui lòng nhập mật khẩu</div>
-            </div>
-            @error('password')
-                <p class="">{{ $message }}</p>
-            @enderror
-            <label>Nhập lại mật khẩu</label>
-            <div class="input-group input-user">
-                <div class="input-group-prepend">
-                    <div class="input-group-text"><i class="fa fa-lock"></i></div>
-                </div>
-                <input type="password" class="form-control text-sm" id="repassword" name="repassword" placeholder="Nhập lại mật khẩu" required="">
-            <div class="invalid-feedback">Vui lòng nhập lại mật khẩu</div>
-            </div>
-            @error('repassword')
-                <p class="">{{ $message }}</p>
-            @enderror
-            <label>Email</label>
-            <div class="input-group input-user">
-                <div class="input-group-prepend">
-                    <div class="input-group-text"><i class="fa fa-envelope"></i></div>
-                </div>
-                <input type="email" class="form-control text-sm" id="email" name="email" placeholder="Nhập địa chỉ email của bạn" value="" required="">
-            <div class="invalid-feedback">Vui lòng nhập địa chỉ email</div>
-            </div>
-            @error('email')
-                <p class="">{{ $message }}</p>
-            @enderror
-            <label>Điện thoại</label>
-            <div class="input-group input-user">
-                <div class="input-group-prepend">
-                    <div class="input-group-text"><i class="fa fa-phone-square"></i></div>
-                </div>
-                <input type="number" class="form-control text-sm" id="phone" name="phone" placeholder="Nhập số điện thoại" value="" required="">
-            <div class="invalid-feedback">Vui lòng nhập số điện thoại</div>
-            </div>
-            @error('phone')
-                <p class="">{{ $message }}</p>
-            @enderror
-            <!--<label>Địa chỉ</label>
+                    <label>Mật khẩu</label>
+                    <div class="input-group input-user">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fa fa-lock"></i></div>
+                        </div>
+                        <input type="password" class="form-control text-sm" id="password" name="password"
+                            placeholder="Nhập mật khẩu (trên 8 kí tự)" required="">
+                        <div class="invalid-feedback">Vui lòng nhập mật khẩu</div>
+                    </div>
+                    @error('password')
+                    <p class="">{{ $message }}</p>
+                    @enderror
+                    <label>Nhập lại mật khẩu</label>
+                    <div class="input-group input-user">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fa fa-lock"></i></div>
+                        </div>
+                        <input type="password" class="form-control text-sm" id="repassword" name="repassword"
+                            placeholder="Nhập lại mật khẩu" required="">
+                        <div class="invalid-feedback">Vui lòng nhập lại mật khẩu</div>
+                    </div>
+                    @error('repassword')
+                    <p class="">{{ $message }}</p>
+                    @enderror
+                    <label>Email</label>
+                    <div class="input-group input-user">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fa fa-envelope"></i></div>
+                        </div>
+                        <input type="email" class="form-control text-sm" id="email" name="email"
+                            placeholder="Nhập địa chỉ email của bạn" value="" required="">
+                        <div class="invalid-feedback">Vui lòng nhập địa chỉ email</div>
+                    </div>
+                    @error('email')
+                    <p class="">{{ $message }}</p>
+                    @enderror
+                    <label>Điện thoại</label>
+                    <div class="input-group input-user">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fa fa-phone-square"></i></div>
+                        </div>
+                        <input type="number" class="form-control text-sm" id="phone" name="phone"
+                            placeholder="Nhập số điện thoại" value="" required="">
+                        <div class="invalid-feedback">Vui lòng nhập số điện thoại</div>
+                    </div>
+                    @error('phone')
+                    <p class="">{{ $message }}</p>
+                    @enderror
+                    <!--<label>Địa chỉ</label>
             <div class="input-group input-user">
                 <div class="input-group-prepend">
                     <div class="input-group-text"><i class="fa fa-map"></i></div>
@@ -126,24 +133,24 @@
                 <input type="text" class="form-control text-sm" id="address" name="address" placeholder="Nhập địa chỉ của bạn" value="" required="">
             <div class="invalid-feedback">Vui lòng nhập địa chỉ</div>
             </div>-->
-            <div class="button-user">
-                <input type="submit" class="btn btn-primary btn-block" name="check-register" value="Đăng ký">
-            </div>
-        </form>
-        <div class="fb-google-btn">
-            <a href="#" class="fb-google-btn--fb btn btn--size-s btn--with-icon">
-                <i class=" fb-google-btn-icon fa-brands fa-square-facebook"></i>
-                <span class="fb-google-btn-title">Kết nối với Facebook</span>
-            </a>
-            <a href="#" class="fb-google-btn--gg btn btn--size-s btn--with-icon">
-                <i class="fb-google-btn-icon fa-brands fa-google"></i>
-                <span class="fb-google-btn-title">Kết nối với Google</span>
+                    <div class="button-user">
+                        <input type="submit" class="btn btn-primary btn-block" name="check-register" value="Đăng ký">
+                    </div>
+                </form>
+                <div class="fb-google-btn">
+                    <a href="#" class="fb-google-btn--fb btn btn--size-s btn--with-icon">
+                        <i class=" fb-google-btn-icon fa-brands fa-square-facebook"></i>
+                        <span class="fb-google-btn-title">Kết nối với Facebook</span>
+                    </a>
+                    <a href="#" class="fb-google-btn--gg btn btn--size-s btn--with-icon">
+                        <i class="fb-google-btn-icon fa-brands fa-google"></i>
+                        <span class="fb-google-btn-title">Kết nối với Google</span>
 
-            </a>
-        </div>
-    </div>
-    
-        <!-- <div class="row justify-content-center">
+                    </a>
+                </div>
+            </div>
+
+            <!-- <div class="row justify-content-center">
             <div class="col-md-4">
                 <div class="card">
                     <div class="card-header">Đăng Ký</div>
@@ -193,7 +200,8 @@
 
 
 
-
+        </div>
+    </div>
     </div>
 
 
--- datn/resources/views/DangNhap.blade.php
+++ datn/resources/views/DangNhap.blade.php
@@ -7,7 +7,8 @@
     <nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
         <div class="container">
             <a class="navbar-brand mr-auto" href="#">Dedicated Dragon BookStore</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
@@ -54,47 +55,53 @@
         </div>
 
         <!-- login -->
-    <div class="wrap-user">
-        <div class="title-user d-flex align-items-end justify-content-between">
-            <span>Đăng nhập</span>
-            <a href="" title="Quên mật khẩu">Quên mật khẩu</a>
+        <div class="wrap-userdn">
+            <div class="box-login">
+                <div class="title-user d-flex align-items-end justify-content-between">
+                    <span>Đăng nhập</span>
+                    <a href="" title="Quên mật khẩu">Quên mật khẩu</a>
+                </div>
+                <form class="form-user validation-user" novalidate="" method="post" action="{{route('check_login')}}"
+                    enctype="">
+                    @csrf
+                    <div class="input-group input-user">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fa fa-user"></i></div>
+                        </div>
+                        <input type="text" class="form-control text-sm" id="username" name="username"
+                            placeholder="Tài khoản" required="">
+                        <div class="invalid-feedback">Vui lòng nhập tài khoản</div>
+                    </div>
+                    <div class="input-group input-user">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text"><i class="fa fa-lock"></i></div>
+                        </div>
+                        <input type="password" class="form-control text-sm" id="password" name="password"
+                            placeholder="Mật khẩu" required="">
+
+                        @error('username')
+                        <p class="">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <div class="button-user d-flex align-items-center justify-content-between">
+                        <!--nút đăng nhập-->
+                        <input type="submit" class="btn btn-primary" name="login-user" value="Đăng nhập">
+                        <!--nút nhớ mật khẩu-->
+                        <div class="checkbox-user custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" name="remember-user" id="remember-user"
+                                value="1">
+                            <label class="custom-control-label" for="remember-user">Nhớ mật khẩu</label>
+                        </div>
+                    </div>
+                    <div class="note-user">
+                        <span>Bạn chưa có tài khoản ! </span>
+                        <a href="#" title="Đăng ký">Đăng ký</a>
+                    </div>
+                </form>
+            </div>
         </div>
-        <form class="form-user validation-user" novalidate="" method="post" action="{{route('check_login')}}" enctype="">
-            @csrf
-            <div class="input-group input-user">
-                <div class="input-group-prepend">
-                    <div class="input-group-text"><i class="fa fa-user"></i></div>
-                </div>
-                <input type="text" class="form-control text-sm" id="username" name="username" placeholder="Tài khoản" required="">
-                <div class="invalid-feedback">Vui lòng nhập tài khoản</div>
-            </div>
-            <div class="input-group input-user">
-                <div class="input-group-prepend">
-                    <div class="input-group-text"><i class="fa fa-lock"></i></div>
-                </div>
-                <input type="password" class="form-control text-sm" id="password" name="password" placeholder="Mật khẩu" required="">
-               
-                @error('username')
-                    <p class="">{{ $message }}</p>
-                @enderror
-            </div>
-            <div class="button-user d-flex align-items-center justify-content-between">
-                <!--nút đăng nhập-->
-                <input type="submit" class="btn btn-primary" name="login-user" value="Đăng nhập">
-                <!--nút nhớ mật khẩu-->
-                <div class="checkbox-user custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" name="remember-user" id="remember-user" value="1">
-                    <label class="custom-control-label" for="remember-user">Nhớ mật khẩu</label>
-                </div>
-            </div>
-            <div class="note-user">
-                <span>Bạn chưa có tài khoản  ! </span>
-                <a href="#" title="Đăng ký">Đăng ký</a>
-            </div>
-        </form>
-    </div>
-    
-    
+
+
 </body>
 
 </html>
